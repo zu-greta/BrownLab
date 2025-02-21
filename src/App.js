95333dbbcaeadb393dbc3a82a83676a0df1cd290
greta_zu <149711822+zu-greta@users.noreply.github.com>
2025-02-20 22:33:39 -0500

    github setup

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World!
+          Hello World! This site is for Brown Lab
         </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
